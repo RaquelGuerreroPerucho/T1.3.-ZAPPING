118fcd69354b9b2f2b6f34c52ea017d1c7595676
RaquelGuerreroPerucho <raquelguerreroperucho@gmail.com>
2023-05-14 20:28:37 +0200

    commit2.Zapping

--- Zapping/src/ZappingTest.java
+++ Zapping/src/ZappingTest.java
@@ -13,4 +13,14 @@ class ZappingTest {
         assertEquals(resultadoReal,resultadoEsperado);
     }
 
+    @Test
+    public void test1(){
+        Zapping z1= new Zapping();
+
+        int resultadoReal=z1.cambioCanal(1,2);
+        int resultadoEsperado = 1;
+
+        assertEquals(resultadoReal,resultadoEsperado);
+    }
+
 }
