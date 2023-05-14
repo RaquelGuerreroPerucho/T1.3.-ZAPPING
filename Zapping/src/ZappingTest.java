51715d3c6896b63cba3c22ace45d73872f739cbd
RaquelGuerreroPerucho <raquelguerreroperucho@gmail.com>
2023-05-14 20:31:19 +0200

    commit3.Zapping

--- Zapping/src/ZappingTest.java
+++ Zapping/src/ZappingTest.java
@@ -3,9 +3,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ZappingTest {
+    Zapping z1= new Zapping();
+
     @Test
     public void test0(){
-        Zapping z1= new Zapping();
 
         int resultadoReal=z1.cambioCanal(0,0);
         int resultadoEsperado = 0;
@@ -15,10 +16,27 @@ class ZappingTest {
 
     @Test
     public void test1(){
-        Zapping z1= new Zapping();
 
         int resultadoReal=z1.cambioCanal(1,2);
         int resultadoEsperado = 1;
+
+        assertEquals(resultadoReal,resultadoEsperado);
+    }
+
+    @Test
+    public void test3(){
+
+        int resultadoReal=z1.cambioCanal(10,30);
+        int resultadoEsperado = 20;
+
+        assertEquals(resultadoReal,resultadoEsperado);
+    }
+
+    @Test
+    public void test4(){
+
+        int resultadoReal=z1.cambioCanal(2,99);
+        int resultadoEsperado = 2;
 
         assertEquals(resultadoReal,resultadoEsperado);
     }
